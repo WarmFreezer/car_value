fd29f39f5eaae0dde205b37e970ba485e06df830
WarmFreezer <thomas.eubank516@gmail.com>
2025-11-13 16:22:16 -0500

    File organization

---
+++ main.py
@@ -0,0 +1,10 @@
+# To add the installed packages to requirements.txt
+#   python -m pip freeze > requirements.txt
+# To install packages from requirements.txt
+#   python -m pip install -r requirements.txt
+# If (venv) is not activated, use:
+#  1. Create Virtual Environment  
+#   python -m venv venv 
+#  2. Activate Virtual Environment
+#   venv\Scripts\activate
+
---
+++ models/knn.py
@@ -0,0 +1,1 @@
+# James AKA Jamie
